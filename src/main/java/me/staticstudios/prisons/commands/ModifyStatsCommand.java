2f9cc8cb56f89f09fd6efcfd2e761ca212b93fb8
Sammster10 <76685474+Sammster10@users.noreply.github.com>
2022-03-10 10:53:40 -0500

    Added the following to the "modstats" command: pmine, rawblocks, votes

--- src/main/java/me/staticstudios/prisons/commands/ModifyStatsCommand.java
+++ src/main/java/me/staticstudios/prisons/commands/ModifyStatsCommand.java
@@ -99,6 +99,56 @@ public class ModifyStatsCommand implements CommandExecutor {
                     default -> sender.sendMessage(CommandUtils.getIncorrectCommandUsageMessage("/modstats blocksmined <who> <add|remove|set|reset> <amount>"));
                 }
             }
+            case "rawblocks", "rawblocksmined" -> {
+                BigInteger amount;
+                try {
+                    amount = new BigInteger(args[3]);
+                } catch (NumberFormatException e) {
+                    sender.sendMessage(CommandUtils.getIncorrectCommandUsageMessage("/modstats rawblocks <who> <add|remove|set|reset> <amount>"));
+                    return false;
+                }
+                switch (args[2].toLowerCase()) {
+                    case "add" -> playerData.addRawBlocksMined(amount);
+                    case "remove" -> playerData.removeRawBlocksMined(amount);
+                    case "set" -> playerData.setRawBlocksMined(amount);
+                    case "reset" -> playerData.setRawBlocksMined(BigInteger.ZERO);
+                    default -> sender.sendMessage(CommandUtils.getIncorrectCommandUsageMessage("/modstats rawblocks <who> <add|remove|set|reset> <amount>"));
+                }
+            }
+            case "pmine", "privatemine" -> {
+                int amount;
+                switch (args[2].toLowerCase()) {
+                    case "set" -> {
+                        try {
+                            amount = new BigInteger(args[3]);
+                        } catch (NumberFormatException e) {
+                            sender.sendMessage(CommandUtils.getIncorrectCommandUsageMessage("/modstats pmine <who> <set> <amount>"));
+                            return false;
+                        }
+                        playerData.setPrivateMineSquareSize(amount);
+                        playerData.setHasPrivateMine(true);
+                        playerData.setPrivateMineMat(Material.STONE);
+                    }
+                    case "reset" -> playerData.setHasPrivateMine(false);
+                    default -> sender.sendMessage(CommandUtils.getIncorrectCommandUsageMessage("/modstats pmine <who> <set|reset>"));
+                }
+            }
+            case "votes", "vote" -> {
+                BigInteger amount;
+                try {
+                    amount = new BigInteger(args[3]);
+                } catch (NumberFormatException e) {
+                    sender.sendMessage(CommandUtils.getIncorrectCommandUsageMessage("/modstats votes <who> <add|remove|set|reset> <amount>"));
+                    return false;
+                }
+                switch (args[2].toLowerCase()) {
+                    case "add" -> playerData.addVotes(amount);
+                    case "remove" -> playerData.removeVotes(amount);
+                    case "set" -> playerData.setVotes(amount);
+                    case "reset" -> playerData.setVotes(BigInteger.ZERO);
+                    default -> sender.sendMessage(CommandUtils.getIncorrectCommandUsageMessage("/modstats votes <who> <add|remove|set|reset> <amount>"));
+                }
+            }
             case "timeplayed" -> {
                 BigInteger amount;
                 try {
