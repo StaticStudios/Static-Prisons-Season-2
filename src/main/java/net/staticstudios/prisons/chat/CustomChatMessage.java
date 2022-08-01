4b0e2d5eebc72d7fcbab9e521f40cf950d844fd5
leguan <longboard.noah@gmail.com>
2022-08-01 11:12:52 +0200

    Fixed bug where console did not receive formatted message

--- src/main/java/net/staticstudios/prisons/chat/CustomChatMessage.java
+++ src/main/java/net/staticstudios/prisons/chat/CustomChatMessage.java
@@ -1,6 +1,7 @@
 package net.staticstudios.prisons.chat;
 
 import io.papermc.paper.event.player.AsyncChatEvent;
+import net.kyori.adventure.audience.Audience;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.JoinConfiguration;
 import net.kyori.adventure.text.event.HoverEvent;
@@ -10,6 +11,7 @@ import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.staticstudios.prisons.UI.tablist.TeamPrefix;
 import net.staticstudios.prisons.data.PlayerData;
 import net.staticstudios.prisons.utils.PrisonUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 
@@ -31,52 +33,54 @@ public class CustomChatMessage {
     }
 
     public void sendFormatted() {
-        event.renderer((source, sourceDisplayName, message, viewer) -> {
-            Component prestige = Component.text("P").append(Component.text(PrisonUtils.prettyNum(playerData.getPrestige()))).color(NamedTextColor.LIGHT_PURPLE);
-            Component chatTag1 = ChatTags.getFromID(playerData.getChatTag1());
-            Component chatTag2 = ChatTags.getFromID(playerData.getChatTag2());
+        Component message = event.message();
+        Player source = event.getPlayer();
+        Component prestige = Component.text("P").append(Component.text(PrisonUtils.prettyNum(playerData.getPrestige()))).color(NamedTextColor.LIGHT_PURPLE);
+        Component chatTag1 = ChatTags.getFromID(playerData.getChatTag1());
+        Component chatTag2 = ChatTags.getFromID(playerData.getChatTag2());
 
-            Component rankTag = TeamPrefix.getFromID(playerData.getTabListPrefixID());
+        Component rankTag = TeamPrefix.getFromID(playerData.getTabListPrefixID());
 
-            HoverEvent<Component> realNameHoverEvent = HoverEvent.showText(Component.text("Real Name: ").color(NamedTextColor.AQUA)
-                    .append(source.name().color(NamedTextColor.WHITE))
-                    .append(Component.newline())
-                    .append(Component.text("Discord Name: ").color(NamedTextColor.AQUA))
-                    .append(Component.text(playerData.getDiscordName()).color(NamedTextColor.WHITE))
-                    .asComponent());
-
-
-            Component prefix = Component.join(JoinConfiguration.noSeparators(),
-                    Component.text("[").color(NamedTextColor.DARK_GRAY).append(prestige).append(Component.text("] ").color(NamedTextColor.DARK_GRAY)),
-                    "".equals(playerData.getChatTag1()) ? Component.empty() : Component.text("[").color(NamedTextColor.DARK_GRAY).append(chatTag1).append(Component.text("] ").color(NamedTextColor.DARK_GRAY)),
-                    "".equals(playerData.getChatTag2()) ? Component.empty() : Component.text("[").color(NamedTextColor.DARK_GRAY).append(chatTag2).append(Component.text("] ").color(NamedTextColor.DARK_GRAY)),
-                    Component.text("[").color(NamedTextColor.DARK_GRAY).append(rankTag).append(Component.text("] ").color(NamedTextColor.DARK_GRAY)),
-                    playerData.getIsChatNicknameEnabled() ? MiniMessage.miniMessage().deserialize(playerData.getChatNickname()) : sourceDisplayName.color(TextColor.fromHexString("#54f08a"))
-            );
-
-            //Component censoredMessage = Component.text(chatFilter(PlainTextComponentSerializer.plainText().serialize(message)));
-            Component censoredMessage = message;
-
-            if ((playerData.getPlayerRank().contains("warrior") || playerData.getIsNitroBoosting()) &&
-                    (!Material.AIR.equals(source.getInventory().getItemInMainHand().getType()) && source.getInventory().getItemInMainHand().getItemMeta().hasDisplayName())) {
-                censoredMessage = censoredMessage.replaceText(builder -> builder.match("\\[item\\]").replacement(
-                        (source.getInventory().getItemInMainHand().getAmount() > 1
-                                ? Component.text("[").append(Component.text(source.getInventory().getItemInMainHand().getAmount())).append(Component.text("x] ")).color(NamedTextColor.AQUA)
-                                : Component.empty())
-                                .append(Objects.requireNonNull(source.getInventory().getItemInMainHand().getItemMeta().displayName()).hoverEvent(
-                                        source.getInventory().getItemInMainHand().asHoverEvent()))
-                ).once());
-
-                censoredMessage = censoredMessage.replaceText(builder -> builder.match("\\[item\\]").replacement(Component.empty()));
-            }
+        HoverEvent<Component> realNameHoverEvent = HoverEvent.showText(Component.text("Real Name: ").color(NamedTextColor.AQUA)
+                .append(source.name().color(NamedTextColor.WHITE))
+                .append(Component.newline())
+                .append(Component.text("Discord Name: ").color(NamedTextColor.AQUA))
+                .append(Component.text(playerData.getDiscordName()).color(NamedTextColor.WHITE))
+                .asComponent());
 
 
+        Component prefix = Component.join(JoinConfiguration.noSeparators(),
+                Component.text("[").color(NamedTextColor.DARK_GRAY).append(prestige).append(Component.text("] ").color(NamedTextColor.DARK_GRAY)),
+                "".equals(playerData.getChatTag1()) ? Component.empty() : Component.text("[").color(NamedTextColor.DARK_GRAY).append(chatTag1).append(Component.text("] ").color(NamedTextColor.DARK_GRAY)),
+                "".equals(playerData.getChatTag2()) ? Component.empty() : Component.text("[").color(NamedTextColor.DARK_GRAY).append(chatTag2).append(Component.text("] ").color(NamedTextColor.DARK_GRAY)),
+                Component.text("[").color(NamedTextColor.DARK_GRAY).append(rankTag).append(Component.text("] ").color(NamedTextColor.DARK_GRAY)),
+                playerData.getIsChatNicknameEnabled() ? MiniMessage.miniMessage().deserialize(playerData.getChatNickname()) : source.displayName().color(TextColor.fromHexString("#54f08a"))
+        );
 
-            return Component.join(JoinConfiguration.noSeparators(),
-                    prefix.hoverEvent(realNameHoverEvent),
-                    Component.text(": "),
-                    censoredMessage.color(playerData.getChatColor()).decorations(playerData.getChatDecorations()));
-        });
+        //Component censoredMessage = Component.text(chatFilter(PlainTextComponentSerializer.plainText().serialize(message)));
+        Component censoredMessage = message;
+
+        if ((playerData.getPlayerRanks().contains("warrior") || playerData.getIsNitroBoosting()) &&
+                (!Material.AIR.equals(source.getInventory().getItemInMainHand().getType()) && source.getInventory().getItemInMainHand().getItemMeta().hasDisplayName())) {
+            censoredMessage = censoredMessage.replaceText(builder -> builder.match("\\[item\\]").replacement(
+                    (source.getInventory().getItemInMainHand().getAmount() > 1
+                            ? Component.text("[").append(Component.text(source.getInventory().getItemInMainHand().getAmount())).append(Component.text("x] ")).color(NamedTextColor.AQUA)
+                            : Component.empty())
+                            .append(Objects.requireNonNull(source.getInventory().getItemInMainHand().getItemMeta().displayName()).hoverEvent(
+                                    source.getInventory().getItemInMainHand().asHoverEvent()))
+            ).once());
+
+            censoredMessage = censoredMessage.replaceText(builder -> builder.match("\\[item\\]").replacement(Component.empty()));
+        }
+
+        Component formattedMessage = Component.join(JoinConfiguration.noSeparators(),
+                prefix.hoverEvent(realNameHoverEvent),
+                Component.text(": ").color(NamedTextColor.DARK_GRAY),
+                censoredMessage.color(playerData.getChatColor()).decorations(playerData.getChatDecorations()));
+
+        Bukkit.getConsoleSender().sendMessage(formattedMessage);
+
+        event.renderer((source1, sourceDisplayName, message1, viewer) -> formattedMessage);
     }
 
     private static final List<String> bannedWords = new ArrayList<>();
