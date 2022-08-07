d21a09a755f42215cffa839966ec15cc7657696e
longb <longboard.noah@gmail.com>
2022-08-07 23:57:06 +0200

    rewrite minebombs

--- src/main/java/net/staticstudios/prisons/customitems/minebombs/MineBombs.java
+++ src/main/java/net/staticstudios/prisons/customitems/minebombs/MineBombs.java
@@ -6,11 +6,9 @@ import net.staticstudios.mines.StaticMine;
 import net.staticstudios.prisons.StaticPrisons;
 import net.staticstudios.prisons.backpacks.PrisonBackpacks;
 import net.staticstudios.prisons.customitems.CustomItem;
-import net.staticstudios.prisons.minebombs.MultiBombMineBomb;
 import net.staticstudios.prisons.minebombs.PreComputerMineBomb;
 import net.staticstudios.prisons.mines.MineBlock;
 import net.staticstudios.prisons.utils.ComponentUtil;
-import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.block.Block;
@@ -22,56 +20,41 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataType;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class MineBombTier1 implements CustomItem {
+public enum MineBombs implements CustomItem {
+    TIER_1(1, "mine_bomb_1", new PreComputerMineBomb(15)),
+    TIER_2(2, "mine_bomb_2", new PreComputerMineBomb(20)),
+    TIER_3(3, "mine_bomb_3", new PreComputerMineBomb(27)),
+    TIER_4(4, "mine_bomb_4", new PreComputerMineBomb(40));
 
-    static NamespacedKey MINE_BOMB_KEY;
-    static NamespacedKey getMineBombKey() {
-        if (MINE_BOMB_KEY == null) {
-            MINE_BOMB_KEY = new NamespacedKey(StaticPrisons.getInstance(), "mineBomb");
-        }
-        return MINE_BOMB_KEY;
-    }
 
-    static ItemStack getMineBomb(int tier, Component name) {
-        ItemStack item = new ItemStack(Material.TNT);
-        ItemMeta meta = item.getItemMeta();
-        meta.getPersistentDataContainer().set(getMineBombKey(), PersistentDataType.INTEGER, tier);
-        meta.displayName(name);
-        meta.lore(
-                List.of(
-                        Component.empty(),
-                        Component.text("Place me in a mine and I'll explode!").color(ComponentUtil.LIGHT_GRAY).decorate(TextDecoration.ITALIC)
-                )
-        );
-        meta.addEnchant(Enchantment.LURE, 1, true);
-        meta.addItemFlags(ItemFlag.HIDE_ENCHANTS, ItemFlag.HIDE_ATTRIBUTES);
-        item.setItemMeta(meta);
-        return item;
-    }
+    private final int tier;
+    private final String id;
+    private final PreComputerMineBomb mineBomb;
 
-    public MineBombTier1() {
+    private static final NamespacedKey MINE_BOMB_KEY = new NamespacedKey(StaticPrisons.getInstance(), "mineBomb");
+    private static final int VIRTUAL_FORTUNE = 500;
+
+    MineBombs(int tier, String id, PreComputerMineBomb mineBomb) {
+        this.tier = tier;
+        this.id = id;
+        this.mineBomb = mineBomb;
         register();
     }
 
     @Override
     public String getID() {
-        return "mine_bomb_1";
+        return id;
     }
 
     @Override
     public ItemStack getItem(Player player) {
-        return getMineBomb(1, Component.empty().append(Component.text("Small Mine Bomb").color(ComponentUtil.YELLOW).decorate(TextDecoration.BOLD)).decoration(TextDecoration.ITALIC, false));
+        return getMineBomb(tier, Component.empty().append(Component.text("Small Mine Bomb").color(ComponentUtil.YELLOW).decorate(TextDecoration.BOLD)).decoration(TextDecoration.ITALIC, false));
     }
-
-    private static final PreComputerMineBomb MINE_BOMB_1 = new PreComputerMineBomb(15);
-    private static final PreComputerMineBomb MINE_BOMB_2 = new PreComputerMineBomb(20);
-    private static final PreComputerMineBomb MINE_BOMB_3 = new PreComputerMineBomb(27);
-    private static final PreComputerMineBomb MINE_BOMB_4 = new PreComputerMineBomb(40);
-    private static final int VIRTUAL_FORTUNE = 500;
 
     @Override //Listener for all Mine bomb tiers, since the listeners would be very similar
     public boolean onInteract(PlayerInteractEvent e) {
@@ -90,21 +73,17 @@ public class MineBombTier1 implements CustomItem {
             return false;
         }
         ItemMeta itemMeta = itemStack.getItemMeta();
-        if (!itemMeta.getPersistentDataContainer().has(getMineBombKey(), PersistentDataType.INTEGER)) {
+        if (!itemMeta.getPersistentDataContainer().has(MINE_BOMB_KEY, PersistentDataType.INTEGER)) {
             return false;
         }
         StaticMine mine = StaticMine.fromLocationXZ(block.getLocation());
         if (mine == null) {
             return false;
         }
-        int bombTier = itemMeta.getPersistentDataContainer().get(getMineBombKey(), PersistentDataType.INTEGER);
-        PreComputerMineBomb mineBomb = null;
-        switch (bombTier) {
-            default -> mineBomb = MINE_BOMB_1;
-            case 2 -> mineBomb = MINE_BOMB_2;
-            case 3 -> mineBomb = MINE_BOMB_3;
-            case 4 -> mineBomb = MINE_BOMB_4;
-        }
+        int bombTier = itemMeta.getPersistentDataContainer().get(MINE_BOMB_KEY, PersistentDataType.INTEGER);
+
+        PreComputerMineBomb mineBomb = getMineBombByTier(bombTier);
+
         Map<MineBlock, Long> map = new HashMap<>();
         for (Map.Entry<Material, Long> entry: mineBomb.explode(mine, block.getLocation()).entrySet()) {
             map.put(MineBlock.fromMaterial(entry.getKey()), entry.getValue() * VIRTUAL_FORTUNE);
@@ -114,4 +93,30 @@ public class MineBombTier1 implements CustomItem {
         PrisonBackpacks.addToBackpacks(e.getPlayer(), map);
         return true;
     }
+
+    private PreComputerMineBomb getMineBombByTier(int bombTier) {
+        return Arrays.stream(values())
+                .filter(mineBomb -> mineBomb.tier == bombTier)
+                .map(mineBombs -> mineBombs.mineBomb)
+                .findFirst().orElse(MineBombs.TIER_1.mineBomb);
+    }
+
+
+    static ItemStack getMineBomb(int tier, Component name) {
+        ItemStack item = new ItemStack(Material.TNT);
+        ItemMeta meta = item.getItemMeta();
+        meta.getPersistentDataContainer().set(MINE_BOMB_KEY, PersistentDataType.INTEGER, tier);
+        meta.displayName(name);
+        meta.lore(
+                List.of(
+                        Component.empty(),
+                        Component.text("Place me in a mine and I'll explode!").color(ComponentUtil.LIGHT_GRAY).decorate(TextDecoration.ITALIC)
+                )
+        );
+        meta.addEnchant(Enchantment.LURE, 1, true);
+        meta.addItemFlags(ItemFlag.HIDE_ENCHANTS, ItemFlag.HIDE_ATTRIBUTES);
+        item.setItemMeta(meta);
+        return item;
+    }
+
 }
